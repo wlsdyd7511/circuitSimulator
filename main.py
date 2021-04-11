eac51d3eaa5f6f616ad572f654449232ff1bcc68
wlsdyd7511 <46102597+wlsdyd7511@users.noreply.github.com>
2021-04-11 15:38:30 +0900

    Make function that find connected elements

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import json
 import sys
+import copy
 
-circuitl = list()
+trackPoint = str()
+trackElement = str()
+
+def find(pin, tracking):
+  temp = list()
+  for i in circuit:
+    if(pin in circuit[i]['pin']):
+      if  i != tracking:
+        temp.append(i)
+  return temp
 
 with open(sys.argv[1], 'r') as f:
   circuit = json.load(f)
 #print(circuit)
+editCircuit = copy.deepcopy(circuit)
+#circuitOut = json.dumps(circuit, indent = 4)
+#print(circuitOut)
+#print(type(circuitOut))
 
-for i in range(0, len(circuit)):
-  circuitl.append(circuit[str(i)])
-  print(circuitl)
+for i in circuit:
+  #print(circuit[i])
+  if(circuit[i]['type'] == 'DCPower'):
+    editCircuit[circuit[i]['pin'][0]] = dict()
+    editCircuit[circuit[i]['pin'][0]][circuit[i]['pin'][1]] = circuit[i]['voltage']
+    trackPoint = circuit[i]['pin'][1]
+    trackElement = i
+    linked = find(trackPoint, trackElement)
+    print(linked)
+
+
+
+
+print(editCircuit)
