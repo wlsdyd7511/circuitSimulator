3aba1208e1f0184b3d7eece95905173c56d90a68
hyuk4673 <hyuk4673@naver.com>
2021-05-05 21:04:17 +0900

    Edit return values to tuple, Define findByiungLyeol

--- main.py
+++ main.py
@@ -23,25 +23,48 @@ def getAdMatrix(cir):
         if (pinList[i] in cir[k]["pin"]) and (pinList[j] in cir[k]["pin"]):
           cnt += 1
       matrix[i][j] = cnt
-  print(matrix)
+  #print(matrix)
+  return((matrix, pinList))
 
-  bmatrix = matrix
+def getBasicMatrix(matrix, pinList):
+  basicMatrix = matrix
+  basicElements = [pin for pin in pinList]
+  numPin = len(pinList)
   pinToDel = list()
   for i in range(numPin):
     cntPin = 0
     for j in range(numPin):
-      cntPin += matrix[i][j]
+     cntPin += matrix[i][j]
     if cntPin == 2:
       pinToDel.append(i)
   pinToDel.reverse()
   cntSeq = 0
   for i in pinToDel:
-    del bmatrix[i]
+    del basicElements[i]
+    del basicMatrix[i]
     cntSeq += 1
     for j in range(numPin - cntSeq):
-      del bmatrix[j][i]
-  print(bmatrix)
-  return(matrix)
+      del basicMatrix[j][i]
+  return((basicMatrix, basicElements))
+
+  def findByoungLyeol(bM, bE, seq):       #basicMatrix, basicElements, seq = 0
+    superBasicMatrix = bM
+    if seq == 0:
+      byoungLyeol = list()
+    endCheck = 1
+    for i in range(len(basicMatrix)):
+      for j in range(len(basicMatrix)):
+        if basicMatrix[i][j] >= 2:
+          superBasicMatrix[i][j] = 1                                 #seq겹 병렬 무시
+          byoungLyeol.append(list())
+          byoungLyeol[seq].append(((bE[i],bE[j]),basicMatrix[i][j])) #seq겹 병렬을 병렬[]의 seq번째 리스트에 ((핀, 핀), connection수)로 저장
+          endCheck = 0
+    if endCheck == 1:
+      return((superBasicMatrix, byoungLyeol))
+    else:
+      basicMatrix = getBasicMatrix(superBasicMatrix, bE)
+      return(findByoungLyeol(basicMatrix[0], basicMatrix[1], seq + 1))
+
 
 
 with open(sys.argv[1], 'r') as f:
@@ -62,5 +85,10 @@ for i in circuit:
 
 
 
-adMatrix = getAdMatrix(circuit)
+ADMatrix = getAdMatrix(circuit)
 #print(editCircuit)
+print(ADMatrix)
+basicMatrix = getBasicMatrix(ADMatrix[0], ADMatrix[1])
+print(basicMatrix)
+superBasicMatrix = findByoungLyeol(basicMatrix[0], basicElements[1], 0)
+print(superBasicMatrix)
